b7cc9b6e9d31826493d6b9fcc36c8c47bbe7a8ec
Santiago Viana <santiagoviana@MacBook-Pro.local>
2022-06-05 06:47:36 -0500

    apuntes sobre directivas [ngStyle] y [ngClass]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MiniCalculadora';
+  titulo = "registro de usuarios";
+  mensaje = "...";
+  registrado = false; 
   numero1:number = 0;
   numero2:number = 0;
   resultado:number = 0;
+  nombre = "";
+  apellido = "";
+  cargo:string = "";
+  entradas: any[]=[
+    {titulo:"python vsfs dìa màs presente"},
+    {titulo:"java desde los 20"},
+    {titulo:"Kotlin más fuerte para tus apps"},
+    {titulo:"web3 se acerca"},
+    {titulo:"Oracle dueño de java"}
+  ];
 
+
+  constructor() {
+    
+  }
+
+  registradoUsuario():void {
+    this.registrado = true;
+    this.mensaje="usuario registrando";
+
+  }
   sumar():void {
 
     this.resultado = this.numero1 + this.numero2;
